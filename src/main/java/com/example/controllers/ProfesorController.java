597be1160c491f879e41c2f4a021a3142424f76c
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-07-02 14:31:15 +0200

    Antes de irme a casa

--- src/main/java/com/example/controllers/ProfesorController.java
+++ src/main/java/com/example/controllers/ProfesorController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.entities.Estudiante;
+// import com.example.entities.Estudiante;
 import com.example.entities.Profesor;
 import com.example.services.FacultadService;
 import com.example.services.ProfesorService;
